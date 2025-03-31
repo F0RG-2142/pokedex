661887bdc22bd3dffe2d24b96958584e33fbe025
Louis <vanaaardtlj03@gmail.com>
2025-03-31 15:42:44 +0200

    stable

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/F0RG-2142/pokedex/wisk"
 )
 
 func main() {
-	fmt.Printf("Hello World!")
-	fmt.Printf(wisk.add(1, 2))
+	fmt.Printf("Hello World!\n")
+	fmt.Printf("%d", wisk.Add(1, 2))
 }
